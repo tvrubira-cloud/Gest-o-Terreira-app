a56a1db287507f2b71b3ba8232f5453e4e000b92
Guaraci <tvrubira@gmail.com>
2026-06-20 17:51:57 -0300

    antes de configurar capgo OTA

---
+++ android/app/src/main/java/com/orunapp/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.orunapp.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.orunapp.app',
+  appName: 'orunapp',
+  webDir: 'dist'
+};
+
+export default config;
